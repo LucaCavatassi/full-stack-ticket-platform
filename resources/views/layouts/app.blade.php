a7b6703416f9336f45115a2d103824484a3002d8
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-20 15:10:59 +0100

    feat: fix dropdown menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Ticket Platform') }}</title>
 
 
     <!-- Fonts -->
@@ -21,22 +21,26 @@
 
 <body>
     <div id="app">
-
-
+        {{-- NavBar --}}
         <nav class="navbar navbar-expand-md navbar-light bg-dark shadow-sm" style="height: 80px">
             <div class="container d-flex justify-content-between">
+                {{-- Brand --}}
                 <a class="navbar-brand d-flex align-items-center text-white" href="{{ url('/') }}">
-                    <div class="logo_laravel">
+                    <div class="fs-2">
                         Ticket Platform
                     </div>
                 </a>
+                {{-- Brand --}}
 
+                {{-- Hamburger --}}
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                {{-- Hamburger --}}
 
+                {{-- Collapsed DropDown --}}
                 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -51,14 +55,13 @@
                         {{-- Logged View - Menu --}}
                             <li class="nav-item dropdown">
                                 {{-- Username toogler for dropdown --}}
-                                <a id="navbarDropdown" class="nav-link text-white dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="fs-4 nav-link text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
                                 {{-- Username toogler for dropdown --}}
 
                                 {{-- Menu --}}
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-right bg-dark fs-4" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item text-white" href="{{ route('admin.tickets.index') }}">
                                         {{ __('Dashboard') }}
                                     </a>
@@ -77,10 +80,12 @@
                         @endguest
                     </ul>
                 </div>
+                {{-- Collapsed DropDown --}}
             </div>
         </nav>
+        {{-- NavBar --}}
 
-        <main class="">
+        <main class="ms-height">
             @yield('content')
         </main>
     </div>
