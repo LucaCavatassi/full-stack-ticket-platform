74fd53dd98403cec30840ff0d09435eec482907f
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-20 16:29:11 +0100

    to_fix: hamburger toggler icon white also in app layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,7 +20,8 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
   <!-- Usando Vite -->
-  @vite(['resources/js/app.js'])
+  @vite(['resources/js/app.js', 'resources/scss/app.scss'])
+
 </head>
 
 <body>
