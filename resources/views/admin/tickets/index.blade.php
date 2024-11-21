99fe1f778c21daa18a474fc8000511b40e075536
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-21 16:34:30 +0100

    feat: anchor instead of buttons and form

--- resources/views/admin/tickets/index.blade.php
+++ resources/views/admin/tickets/index.blade.php
@@ -21,16 +21,12 @@
         <p>No tickets available.</p>
     @else
         <h1 class="pb-3">All tickets</h1>
-        <form method="GET" action="{{ route('admin.tickets.filter') }}">
-            <div class="btn-group">
-                <button type="submit" class="btn btn-primary" name="status_id" value="1">Assegnato</button>
-                <button type="submit" class="btn btn-primary" name="status_id" value="2">In Lavorazione</button>
-                <button type="submit" class="btn btn-primary" name="status_id" value="3">Chiuso</button>
-                <form method="GET" action="{{route('admin.tickets.index')}}">
-                    <button type="submit" class="btn btn-primary">Tutti</button>
-                </form>
-            </div>
-        </form>
+        <div class="btn-group">
+            <a href="{{ route('admin.tickets.filter', ['status_id' => 1]) }}" class="btn btn-primary {{ request('status_id') == 1 ? 'active' : '' }}">Assegnato</a>
+            <a href="{{ route('admin.tickets.filter', ['status_id' => 2]) }}" class="btn btn-primary {{ request('status_id') == 2 ? 'active' : '' }}">In Lavorazione</a>
+            <a href="{{ route('admin.tickets.filter', ['status_id' => 3]) }}" class="btn btn-primary {{ request('status_id') == 3 ? 'active' : '' }}">Chiuso</a>
+            <a href="{{ route('admin.tickets.index') }}" class="btn btn-primary">Tutti</a>
+        </div>
 
         @foreach ($tickets as $ticket)
             <div class="card mb-3">
